8b0fc1832e5a659b6ac7166d6f637fbae5d2d9b4
pbedrosian <peter_bedrosian@Yahoo.com>
2020-05-06 18:09:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+ 
+    get '/newteam' do
+        puts params
+        erb :newteam
+    end 
 
+    post '/newteam' do
+        @new_team = params
+
+        erb :team
+    end
 
 end
