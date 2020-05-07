9830e89b361f4adf3bd2d9b27b3cb1d8515daa17
sgrant0225 <sharnellgrant@gmail.com>
2020-05-06 21:13:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 require 'pry'
 class App < Sinatra::Base
  
-   get '/newteam' do 
+   get '/newteam' do #this connects to the form (route) 
     erb :newteam
     end   
     
-    post '/team' do 
+    post '/team' do #once you fill the form it will post or display the form with the information
         @name = params["name"]
         @coach = params["coach"]
         @pg = params["pg"]
